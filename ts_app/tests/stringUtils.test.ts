d40395fab8ba89e266a04eed3c0c4991e23fa4b4
gabriela611 <gsanabriag@eafit.edu.co>
2026-03-04 10:28:54 -0500

    typescript corrected test and files

--- ts_app/tests/stringUtils.test.ts
+++ ts_app/tests/stringUtils.test.ts
@@ -4,8 +4,32 @@ import { isPalindrome } from '../src/stringUtils'
 
 // TODO: completa los casos: "radar"->true, "anita lava la tina"->true, "python"->false, ""->true, "Radar"->true
 
-describe('siembra', () => {
-  it('ejemplo', () => {
-    expect(true).toBe(true)
+describe('radar palindromo', () => {
+  it('should return true', () => {
+    expect(isPalindrome('radar')).toBe(true)
+  })
+})
+
+describe('Radar palindromo', () => {
+  it('should return true', () => {
+    expect(isPalindrome('Radar')).toBe(true)
+  })
+})
+
+describe('anita palindromo', () => {
+  it('should return true', () => {
+    expect(isPalindrome('anita lava la tina')).toBe(true)
+  })
+})
+
+describe('python palindromo', () => {
+  it('should return false', () => {
+    expect(isPalindrome('python')).toBe(false)
+  })
+})
+
+describe('empty palindromo', () => {
+  it('should return true', () => {
+    expect(isPalindrome(' ')).toBe(true)
   })
 })
